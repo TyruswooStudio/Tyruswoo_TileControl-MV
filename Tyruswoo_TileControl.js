2654267d79c848603d9d1954c9f7cc324dcad219
McKathlin <McKathlin@gmail.com>
2023-09-18 22:04:56 -0400

    Add MV to plugindesc of Tyruswoo_TileControl.js

--- Tyruswoo_TileControl.js
+++ Tyruswoo_TileControl.js
@@ -37,7 +37,7 @@ var Tyruswoo = Tyruswoo || {};
 Tyruswoo.TileControl = Tyruswoo.TileControl || {};
 
 /*:
- * @plugindesc v1.0.1  Allows greater control of tiles and tilesets.
+ * @plugindesc MV v1.0.1  Allows greater control of tiles and tilesets.
  * @author Tyruswoo
  *
  * @param Tile Info on Move
